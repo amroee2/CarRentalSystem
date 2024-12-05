f009b1360a793679f5e3e1759b53a435b8d02603
Amro Qadadha <amroqadadha@gmail.com>
2024-12-05 17:39:25 +0200

    Create Admin and Rental entities

---
+++ CarRentalSystem/Models/Rental.cs
@@ -0,0 +1,13 @@
+﻿namespace CarRentalSystem.Models
+{
+    public class Rental
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public User User { get; set; }
+        public int CarId { get; set; }
+        public Car Car { get; set; }
+        public DateTime RentalStartDate { get; set; }
+        public DateTime RentalEndDate { get; set; }
+    }
+}
