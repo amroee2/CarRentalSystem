7c490c2e51e3001a98da0493efb23d7acf6cf3cc
Amro Qadadha <amroqadadha@gmail.com>
2024-12-05 20:53:00 +0200

    Add a new page for car details

---
+++ CarRentalSystem/Controllers/CarController.cs
@@ -0,0 +1,21 @@
+﻿using CarRentalSystem.Models.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRentalSystem.Controllers
+{
+    public class CarController : Controller
+    {
+        private readonly ICarRepository _carRepository;
+        private readonly ILogger<HomeController> _logger;
+
+        public CarController(ICarRepository carRepository, ILogger<HomeController> logger)
+        {
+            _carRepository = carRepository;
+            _logger = logger;
+        }
+        public async Task<IActionResult> Details(int id)
+        {
+            return View(await _carRepository.GetCar(id));
+        }
+    }
+}
