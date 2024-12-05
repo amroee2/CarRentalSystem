dc4cf2e0622192ed015405bec65ce0aaef3a177c
Amro Qadadha <amroqadadha@gmail.com>
2024-12-05 20:12:23 +0200

    Add CarListViewModel

---
+++ CarRentalSystem/ViewModels/CarListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CarRentalSystem.Models;
+
+namespace CarRentalSystem.ViewModels
+{
+    public class CarListViewModel
+    {
+
+        public List<Car> Cars { get; set; }
+        public CarListViewModel(List<Car> cars)
+        {
+            Cars = cars;
+        }
+    }
+}
