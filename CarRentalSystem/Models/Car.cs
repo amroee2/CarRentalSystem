84d773ea2348e728842f11e14a06ae4f622e16f1
Amro Qadadha <amroqadadha@gmail.com>
2024-12-05 17:38:58 +0200

    Create User and Car entities

---
+++ CarRentalSystem/Models/Car.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarRentalSystem.Models
+{
+    public class Car
+    {
+        [Key]
+        public int CarId { get; set; }
+        [Required(ErrorMessage = "Please enter a Car Name")]
+        [Display(Name = "Car Name")]
+        public string CarName { get; set; }
+        [Required(ErrorMessage = "Please enter a Car Model")]
+        [Display(Name = "Car Model")]
+        public string CarModel { get; set; }
+        [Required(ErrorMessage = "Please enter a Car Image")]
+        [Display(Name = "Car Image")]
+        public string CarImage { get; set; }
+        [Required(ErrorMessage = "Please enter a Car Description")]
+        public string CarDescription { get; set; }
+        [Required(ErrorMessage = "Please enter a Car Price")]
+        public double RentPricePerDay { get; set; }
+        [Required(ErrorMessage = "Please enter Car Availability")]
+        public bool IsAvailable { get; set; }
+
+        public List<Rental> Rentals { get; set; }
+
+        public Car()
+        {
+            Rentals = new List<Rental>();
+        }
+    }
+}
---
+++ CarRentalSystem/Models/User.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarRentalSystem.Models
+{
+    public class User : IdentityUser
+    {
+        [Required(ErrorMessage = "Please enter your first Name")]
+        [Display(Name = "First Name")]
+        [StringLength(20)]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Please enter your last Name")]
+        [Display(Name = "Last Name")]
+        [StringLength(20)]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Please enter a valid Email")]
+        [Display(Name = "Email Address")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter a valid Password")]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Please enter your Phone Number")]
+        [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+        [Required(ErrorMessage = "Please enter your Address")]
+        [Display(Name = "Primary Address")]
+        public string PrimaryAddress { get; set; }
+        [Display(Name = "Secondary Address")]
+        public string SecondaryAddress { get; set; }
+        [Required(ErrorMessage = "Please enter your City")]
+        [Display(Name = "City")]
+        public string City { get; set; }
+        [Required(ErrorMessage = "Please enter your Country")]
+        [Display(Name = "Country")]
+        public string Country { get; set; }
+        [Required(ErrorMessage = "Please enter your Driver License Number")]
+        [Display(Name = "Drivers License Number")]
+        public string DriversLicenseNumber { get; set; }
+
+        public List<Rental> Rentals { get; set; }
+
+        public User()
+        {
+            Rentals = new List<Rental>();
+        }
+    }
+}
