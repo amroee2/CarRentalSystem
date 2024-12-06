547046bec6988663eaadf335298beed413008451
Amro Qadadha <amroqadadha@gmail.com>
2024-12-06 18:46:22 +0200

    Add the ability to see Previously Rented Cars

---
+++ CarRentalSystem/Controllers/RentalController.cs
@@ -0,0 +1,26 @@
+﻿using CarRentalSystem.Models;
+using CarRentalSystem.Models.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRentalSystem.Controllers
+{
+    public class RentalController : Controller
+    {
+        private readonly IRentalRepository _rentalRepository;
+        private readonly UserManager<User> _userManager;
+
+        public RentalController(IRentalRepository rentalRepository, UserManager<User> userManager)
+        {
+            _rentalRepository = rentalRepository;
+            _userManager = userManager;
+        }
+
+        public IActionResult UserRentals()
+        {
+            var userId = _userManager.GetUserId(User);
+            var rentals = _rentalRepository.GetAll(userId);
+            return View(rentals);
+        }
+    }
+}
