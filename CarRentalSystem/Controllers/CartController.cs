c7ff0673f830d2bbe169e55f1b08618bf7871305
Amro Qadadha <amroqadadha@gmail.com>
2024-12-06 17:20:51 +0200

    Create CartController

---
+++ CarRentalSystem/Controllers/CartController.cs
@@ -0,0 +1,57 @@
+﻿using CarRentalSystem.Models;
+using CarRentalSystem.Models.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRentalSystem.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartRepository _cartRepository;
+        private readonly UserManager<User> _userManager;
+
+        public CartController(ICartRepository cartRepository, UserManager<User> userManager)
+        {
+            _cartRepository = cartRepository;
+            _userManager = userManager;
+        }
+
+        public IActionResult Checkout()
+        {
+            var userId = _userManager.GetUserId(User);
+            var cart = _cartRepository.GetOrCreateCart(userId);
+            return View(cart);
+        }
+
+        public IActionResult EmptyCart()
+        {
+            var userId = _userManager.GetUserId(User);
+            _cartRepository.EmptyCart(userId);
+            return RedirectToAction("Checkout");
+        }
+
+        [HttpPost]
+        public IActionResult AddToCart(int carId,DateTime start, DateTime end)
+        {
+            var userId = _userManager.GetUserId(User);
+            _cartRepository.AddToCart(userId, carId, start, end);
+            return RedirectToAction("Checkout");
+        }
+
+        [HttpPost]
+        public IActionResult CheckoutCart()
+        {
+            var userId = _userManager.GetUserId(User);
+            _cartRepository.Checkout(userId);
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public IActionResult RemoveFromCart(int carId)
+        {
+            var userId = _userManager.GetUserId(User);
+            _cartRepository.RemoveFromCart(userId, carId);
+            return RedirectToAction("Checkout");
+        }
+    }
+}
