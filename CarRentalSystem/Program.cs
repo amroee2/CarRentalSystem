a7ffb1ce31595ca82c9dd8f1a3fdd2e76561640e
Amro Qadadha <amroqadadha@gmail.com>
2024-12-07 10:16:25 +0200

    Add new role for Admins

--- CarRentalSystem/Program.cs
+++ CarRentalSystem/Program.cs
@@ -7,7 +7,9 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 
 builder.Services.AddDbContext<CarRentalSystemDbContext>(options => options.UseSqlServer(connectionString));
 
-builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<CarRentalSystemDbContext>();
+builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<CarRentalSystemDbContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -15,21 +17,74 @@ builder.Services.AddScoped<ICarRepository, CarRepository>();
 builder.Services.AddScoped<CarRentalSystemDbContext, CarRentalSystemDbContext>();
 builder.Services.AddScoped<ICartRepository, CartRepository>();
 builder.Services.AddScoped<IRentalRepository, RentalRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+async Task SeedAdminRoleAsync(IServiceProvider serviceProvider)
+{
+    var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+    if (!await roleManager.RoleExistsAsync("Admin"))
+    {
+        await roleManager.CreateAsync(new IdentityRole("Admin"));
+    }
+}
+async Task SeedAdminUserAsync(IServiceProvider serviceProvider)
+{
+    var userManager = serviceProvider.GetRequiredService<UserManager<User>>();
+
+    string adminEmail = "admin2@gmail.com";
+    string adminPassword = "Admin@12345";
+    var existingUser = await userManager.FindByEmailAsync(adminEmail);
+
+    if (existingUser == null)
+    {
+        var adminUser = new User
+        {
+            UserName = adminEmail,
+            Email = adminEmail,
+            FirstName = "Admin",
+            LastName = "User",
+            PrimaryAddress = "123 Admin St.",
+            City = "AdminCity",
+            Country = "AdminLand",
+            DriversLicenseNumber = "ADMIN-123456",
+            EmailConfirmed = true,
+            PhoneNumberConfirmed = true,
+            PhoneNumber = "1234567890"
+        };
+
+        var result = await userManager.CreateAsync(adminUser, adminPassword);
+
+        if (result.Succeeded)
+        {
+            await userManager.AddToRoleAsync(adminUser, "Admin");
+        }
+        else
+        {
+            throw new Exception("Failed to create the admin user: " + string.Join(", ", result.Errors.Select(e => e.Description)));
+        }
+    }
+}
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    await SeedAdminRoleAsync(services);
+    await SeedAdminUserAsync(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthentication();
 app.UseAuthorization();
 
@@ -39,3 +94,4 @@ app.MapControllerRoute(
 app.MapRazorPages();
 
 app.Run();
+
