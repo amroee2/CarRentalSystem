b393a34f9ab6ae209284f70f948abde19b5112e0
Amro Qadadha <amroqadadha@gmail.com>
2024-12-07 10:17:30 +0200

    Create new Admin Controller

---
+++ CarRentalSystem/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using CarRentalSystem.Models.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRentalSystem.Controllers
+{
+    public class AdminController: Controller
+    {
+
+        private readonly IUserRepository _userRepository;
+
+        public AdminController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<IActionResult> Users()
+        {
+            var users = await _userRepository.GetUsersAsync();
+            return View(users);
+        }
+    }
+}
